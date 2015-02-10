bf0815d2bb2ff9bdc95cf9f6fdc4193d5847705f
Jooyung Han <jooyung.han@lge.com>
2015-02-10 14:26:39 +0900

    Implements lots of List functions

--- src/main/scala/fpinscala/mylist/MyList.scala
+++ src/main/scala/fpinscala/mylist/MyList.scala
@@ -11,6 +11,9 @@ object ListMain {
 
     def tail: List[A]
 
+    def ++[B >: A](bs: List[B]): List[B] =
+      append(this, bs)
+
     override def toString: String = {
       val result = new ListBuffer[A]
       var cur = this
@@ -30,7 +33,7 @@ object ListMain {
     def tail: List[Nothing] = throw new NoSuchElementException
   }
 
-  case class Cons[A](head: A, tail: List[A]) extends List[A] {
+  case class Cons[+A](head: A, tail: List[A]) extends List[A] {
     val isEmpty = false
   }
 
@@ -40,39 +43,98 @@ object ListMain {
       else Cons(as.head, apply(as.tail: _*))
   }
 
-  def sum(ints: List[Int]): Int = ???
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(h, t) => h + sum(t)
+  }
 
-  def product(ints: List[Int]): Int = ???
+  def product(ints: List[Int]): Int = ints match {
+    case Nil => 1
+    case Cons(h, t) => h * product(t)
+  }
 
   def map[A, B](as: List[A])(f: A => B): List[B] = as match {
     case Nil => Nil
     case Cons(h, t) => Cons(f(h), map(t)(f))
   }
 
-  def filter[A](as: List[A])(f: A => Boolean): List[A] = ???
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = ???
 
-  def append[A](a1: List[A], a2: List[A]): List[A] = ???
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Nil => Nil
+    case Cons(h,t) if f(h) => Cons(h, filter(t)(f))
+    case Cons(_, t) => filter(t)(f)
+  }
 
-  def flatten[A](ass: List[List[A]]): List[A] = ???
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => a2
+    case Cons(h,t) => Cons(h, append(t, a2))
+  }
 
-  def length[A](as: List[A]): Int = ???
+  def flatten[A](ass: List[List[A]]): List[A] = ass match {
+    case Nil => Nil
+    case Cons(as, t) => append(as, flatten(t))
+  }
 
-  def head[A](as: List[A]): A = ???
+  def length[A](as: List[A]): Int = as match {
+    case Nil => 0
+    case Cons(_, t) => 1 + length(t)
+  }
 
-  def tail[A](as: List[A]): List[A] = ???
+  def head[A](as: List[A]): A = as.head
 
-  def take[A](as: List[A], n: Int): List[A] = ???
+  def tail[A](as: List[A]): List[A] = as.tail
 
-  def drop[A](as: List[A], n: Int): List[A] = ???
+  def take[A](as: List[A], n: Int): List[A] =
+    if (n <= 0) Nil
+    else Cons(head(as), take(tail(as), n-1))
 
-  def takeWhile[A](as: List[A])(f: A => Boolean): List[A] = ???
+  def drop[A](as: List[A], n: Int): List[A] =
+    if (n <= 0) as
+    else drop(as.tail, n-1)
 
-  def dropWhile[A](as: List[A])(f: A => Boolean): List[A] = ???
+  def takeWhile[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Cons(h, t) if f(h) => Cons(h, takeWhile(t)(f))
+    case _ => Nil
+  }
+
+  def dropWhile[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Cons(h, t) if f(h) => dropWhile(t)(f)
+    case Nil => Nil
+    case _ => as
+  }
+
+  def zip[A, B](as: List[A], bs: List[B]): List[(A, B)] = (as, bs) match {
+    case (Cons(x,xs), Cons(y,ys)) => Cons((x, y), zip(xs, ys))
+    case _ => Nil
+  }
+
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = (as, bs) match {
+    case (Cons(x,xs), Cons(y,ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+    case _ => Nil
+  }
+
+  def group[A](as: List[A]): List[List[A]] = as match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(takeWhile(as)(h == _), group(dropWhile(as)(h == _)))
+  }
+
+
+
+
 
   def main(args: Array[String]): Unit = {
     val l = List(1, 2, 3)
     require(map(l)(_ + 1) == List(2, 3, 4))
     require(map(l)(_.toString) == List("1", "2", "3"))
     require(filter(l)(_ % 2 == 0) == List(2))
+
+    require(sum(List(1,2,3,4)) == 10)
+
+    require(append(List(1,2,3), List(4,5)) == List(1,2,3,4,5))
+    require(flatten(List(List(1,2,3), List(4,5))) == List(1,2,3,4,5))
+
+
+    require(dropWhile(List(1,2,3,4,5))(n => n < 3) == List(3,4,5))
   }
 }
